8dea07032f38ac80ad3a5273e10aa6b8bf19aaf3
Mariana Martinez Celis <marianamtzcelis19@gmail.com>
2022-04-05 16:52:05 -0500

    Added Packing interface

--- src/fastfood/builder/FastFoodBuilder.java
+++ src/fastfood/builder/FastFoodBuilder.java
@@ -16,7 +16,7 @@ public class FastFoodBuilder {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("hola");
+        
     }
     
 }
