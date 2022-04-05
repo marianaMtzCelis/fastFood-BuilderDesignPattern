b73c10912aa791213c2ac267fdd51fcab2dff253
Mariana Martinez Celis <marianamtzcelis19@gmail.com>
2022-04-05 16:55:25 -0500

    Added Bottle class that implements Packing interface

---
+++ src/fastfood/builder/Bottle.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+
+Recuperado de: https://www.tutorialspoint.com/design_pattern/builder_pattern.htm
+ */
+package fastfood.builder;
+
+/**
+ *
+ * @author marianamtzcelis
+ * Recuperado de: https://www.tutorialspoint.com/design_pattern/builder_pattern.htm
+ */
+public class Bottle implements Packing {
+
+   @Override
+   public String pack() {
+      return "Bottle";
+   }
+}
