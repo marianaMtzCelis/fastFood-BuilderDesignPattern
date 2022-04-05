abcea31e0bee2bf3580d77d24b5737df21a0621c
Mariana Martinez Celis <marianamtzcelis19@gmail.com>
2022-04-05 16:49:17 -0500

    Initial commit

---
+++ src/fastfood/builder/FastFoodBuilder.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+
+Recuperado de: https://www.tutorialspoint.com/design_pattern/builder_pattern.htm
+ */
+package fastfood.builder;
+
+/**
+ *
+ * @author marianamtzcelis
+ */
+public class FastFoodBuilder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("hola");
+    }
+    
+}
